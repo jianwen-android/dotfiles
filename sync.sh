c5dc652f2bbce1dbe147be220c5a34e1bcf8d499
Chin Jian Wen <chin_jian_wen@s2019.ssts.edu.sg>
2021-06-07 20:52:51 +0800

    20:52:51, 07 June 2021

--- sync.sh
+++ sync.sh
@@ -11,24 +11,25 @@ fi
 # Sync sway configs
 linux() {
     # Unique linux config files
-    cp -i $HOME/.config/sway/config sway/
-    cp -i -R $HOME/.config/waybar .
-    cp -i -R $HOME/.config/rofi .
+    cp -i "$HOME"/.config/sway/config sway/
+    cp -i -R "$HOME"/.config/waybar .
+    cp -i -R "$HOME"/.config/rofi .
 
     # Terminal setup
-    cp -i $HOME/.zshrc zshrc/"$os"/zshrc
-    cp -i $HOME/.hyper.js hyper/"$os"
-    cp -i $HOME/.vimrc vim/"$os"
+    cp -i "$HOME"/.zshrc zshrc/"$os"/zshrc
+    cp -i "$HOME"/.hyper.js hyper/"$os"
+    cp -i "$HOME"/.vimrc vim/"$os"
 }
 
 macos() {
     # Terminal setup
-    cp -i $HOME/.zshrc zshrc/"$os"/zshrc
-    cp -i $HOME/.hyper.js hyper/"$os"
-    cp -i $HOME/.vimrc vim/"$os"
+    cp -i "$HOME"/.zshrc zshrc/"$os"/zshrc
+    cp -i "$HOME"/.hyper.js hyper/"$os"
+    cp -i "$HOME"/.vimrc vim/"$os"
 }
 
 # Commit and sync to github :)
 git add .
-git commit -m "`date '+%T, %d %B %Y'`"
+git commit -m "$(date '+%T, %d %B %Y')"
 git push
+echo "Copied new files and synced!"
