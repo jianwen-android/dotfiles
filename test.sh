8a2077dc5c886ed015ac9d85d99850136b2330a2
Chin Jian Wen <chin_jian_wen@s2019.ssts.edu.sg>
2021-06-09 20:12:16 +0800

    tearing hair out now => lazy af later

--- test.sh
+++ test.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-read -p "We good boss? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
+case $REPLY in
+    "hyper") ydiff "$HOME"/.hyper.js ./hyper/"$os"/hyper.js
+    ;;
+    "zshrc") ydiff "$HOME"/.zshrc ./zshrc/"$os"/zshrc
+    ;;
+    "p10k") ydiff "$HOME"/.p10k.zsh ./hyper/"$os"/p10k.zsh
+    ;;
+    "vimrc") ydiff "$HOME"/.vimrc ./vimrc/"$os"/vimrc
+    ;;
+    *) echo "Invalid input, try again"
+esac
+
+case $REPLY in
+    "sway") ydiff "$HOME"/.config/sway ./sway
+    ;;
+    "waybar") ydiff "$HOME"/.config/waybar ./waybar
+
+    "hyper") ydiff "$HOME"/.hyper.js ./hyper/"$os"/hyper.js
+    ;;
+    "zshrc") ydiff "$HOME"/.zshrc ./zshrc/"$os"/zshrc
+    ;;
+    "p10k") ydiff "$HOME"/.p10k.zsh ./hyper/"$os"/p10k.zsh
+    ;;
+    "vimrc") ydiff "$HOME"/.vimrc ./vimrc/"$os"/vimrc
+    ;;
+    *) echo "Invalid input, try again"
+esac
