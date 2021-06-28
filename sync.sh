3dfc41f7e9d3a278bab0d72e08aaf5064c7b1823
Chin Jian Wen <chin_jian_wen@s2019.ssts.edu.sg>
2021-06-28 23:12:39 +0800

    Changed naming scheme for better "code practice" in sync script

--- sync.sh
+++ sync.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# shellcheck source=test.sh
-# source test.sh
 declare -a changedFiles
-# declare -a noSync
+declare -a noSync
 
 #OS check
 if [ "$(uname -s)" = "Darwin" ]; then
@@ -13,13 +11,14 @@ elif [ "$(uname -s)" = "Linux" ]; then
 fi
 
 if [[ $(ydiff "$HOME"/.hyper.js ./hyper/"$os"/hyper.js) ]]; then
-    changedFiles+=("hyper"); fi
+    changedFiles+=("hyper.js"); fi
 if [[ $(ydiff "$HOME"/.zshrc ./zshrc/"$os"/zshrc) ]]; then
     changedFiles+=("zshrc"); fi
 if [[ $(ydiff "$HOME"/.p10k.zsh ./p10k/"$os"/p10k.zsh) ]]; then
-    changedFiles+=("p10k"); fi
+    changedFiles+=("p10k.zsh"); fi
 if [[ $(ydiff "$HOME"/.vimrc ./vimrc/"$os"/vimrc) ]]; then
     changedFiles+=("vimrc"); fi
+if [[ $(ydiff "$HOME"/spicetify_data .//"$os"/spicetify)]]
 
 if [[ "$os" = linux ]]; then
     if [[ $(ydiff "$HOME"/.config/sway ./sway) ]]; then
@@ -50,11 +49,11 @@ while :; do
     else
         file="${changedFiles["$REPLY"]}"
             case $file in
-                "hyper") ydiff ./hyper/"$os"/hyper.js "$HOME"/.hyper.js
+                "hyper.js") ydiff ./hyper/"$os"/hyper.js "$HOME"/.hyper.js
                 ;;
                 "zshrc") ydiff ./zshrc/"$os"/zshrc "$HOME"/.zshrc
                 ;;
-                "p10k") ydiff ./hyper/"$os"/p10k.zsh "$HOME"/.p10k.zsh
+                "p10k.zsh") ydiff ./hyper/"$os"/p10k.zsh "$HOME"/.p10k.zsh
                 ;;
                 "vimrc") ydiff ./vimrc/"$os"/vimrc "$HOME"/.vimrc
                 ;;
@@ -71,7 +70,7 @@ while :; do
     fi
 done
 
-: '
+
 # Terminal setup
 echo
 echo "Files to exclude?"
@@ -82,48 +81,24 @@ do
     noSync+=("${changedFiles["$number"]}")
 done
 
-# shellcheck source=test.sh
-subtractArray
+for file in "${changedFiles[@]}" ; do
+    if  [[ ! $file = "sway" ||  ! $file = "waybar" || ! $file = "rofi" ]]; then
+        if [[ ! "${noSync[@]}" =~ "${file}" ]]; then # if the file is not excluded
+            echo "Syncing $file"
+        fi
+    elif [[ $file = "betterdiscord" ]]; then
+        echo "Syncing betterdiscord"
+    else
+        echo "Syncing $file"
+    fi
+done
 
-'
-
-rsync -azPu "$HOME"/.zshrc zshrc/"$os"/zshrc
-rsync -azPu "$HOME"/.hyper.js hyper/"$os"/hyper.js
-rsync -azPu "$HOME"/.vimrc vimrc/"$os"/vimrc
-rsync -azPu "$HOME"/.p10k.zsh p10k/"$os"/p10k.zsh
-# bro ok
-
-if [[ "$os" = linux ]]; then
-    rsync -azPu "$HOME"/.config/sway "$HOME"/.config/waybar "$HOME"/.config/rofi .
-    rsync -azPu "$HOME"/.config/BetterDiscord/plugins "$HOME"/.config/BetterDiscord/themes ./betterdiscord
-fi
-
-# Insert copying code here
-
+# Commit
 git add .
-git commit -m "$(date '+%T, %d %B %Y')"
+git commit -m "$(date '+%a %d %b %I:%M %p')"
 git push
 echo ""
 echo "Copied new files and synced!"
 
-# Heres how it goes
-# COMPARE ALL FILES BY YDIFF
-# IF OS IS LINUX
-#     DO YOUR EXTRA STEPS
-#
-# COUNT NUMBER OF DIFFS + WHICH ONES ARE DIFFS
-#     NO DIFFS: EXIT 1
-# PRINT FILE + INDEX
-# ASK IF I SHOULD SHOW WHAT ARE DIFFS?
-#     YES: WHICH ONE BABE?
-#         TAKE INDEX, SLAP INTO ARRAY
-#         RETURNS YDIFF OUTPUT
-#         LOOP
-#     NO: PASS
-# COPY
-# EXIT 1
-
-
-
 #TODO you fat shit
-add spicetify
+# add spicetify
--- test.sh
+++ test.sh
@@ -1,5 +1,28 @@
-#!/bin/zsh
+#!/bin/bash
 
-subtractArray () {
-    listr=("${(@)changedFiles:|noSync}")
-}
+declare -a changedFiles
+declare -a noSync
+
+changedFiles=("hyper.js" "betterdiscord" "zshrc" "vimrc")
+noSync=("hyper.js")
+
+echo
+echo "Files to exclude?"
+read -p "[N]one [0-...]: " -r
+echo
+for number in $REPLY
+do
+    noSync+=("${changedFiles["$number"]}")
+done
+
+for file in "${changedFiles[@]}" ; do
+    if  [[ ! $file = "sway" ||  ! $file = "waybar" || ! $file = "rofi" ]]; then
+        if [[ ! "${noSync[@]}" =~ "${file}" ]]; then # if the file is not excluded
+            echo "Syncing $file"
+        fi
+    elif [[ $file = "betterdiscord" ]]; then
+        echo "Syncing betterdiscord"
+    else
+        echo "Syncing $file"
+    fi
+done
